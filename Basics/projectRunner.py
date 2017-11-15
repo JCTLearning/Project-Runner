db46462a139536ded61051fb201b89797e763fbe
JCTLearning <33255294+JCTLearning@users.noreply.github.com>
2017-11-15 08:49:23 -0500

    Added team adv

--- Basics/projectRunner.py
+++ Basics/projectRunner.py
@@ -1,7 +1,7 @@
 import os
 import json
 from tkinter import *
-
+import sqlite3 as lite
 
 
 class gui:
@@ -57,7 +57,7 @@ class gui:
 		self.mainPageBut.grid(column = 0, row = 0)
 		self.teamDisplayFrame.grid(column = 1, row = 0)
 		self.runnersFrame.grid(column = 1, row = 1)
-		self.teamStatsFrame.grid(column = 0, row = 0)
+		self.teamStatsFrame.grid(column = 0, row = 1)
 		"""
 		Main Page Call Back
 		"""
@@ -66,35 +66,47 @@ class gui:
 		"""
 		Team display	
 		"""
+
 		dataC = data()
-		self.teamId = dataC.getTeamId()
-		self.teamBanner = Label(self.teamDisplayFrame, text= self.teamId)
+		self.teamId = dataC.fetchTeamId()
+
+		self.teamBanner = Label(self.teamDisplayFrame, text= 'Team ID: '+self.teamId)
 		self.teamBanner.pack()
 		"""
 		Runers display
 		"""
 		self.rowNum = 0
-		self.runnerId = 1
-		self.runners = dataC.getRunners()
-
-		for items in self.runners:
-			self.runnerIdStr = str(self.runnerId)
-			self.txt = 'Runner ID: '+self.runnerIdStr+' Name: '+items
-			if(items!='null'):
-				"""
-				locals()['runners%runnerId' % self.runnerId] -- Creates a Var name based around the ID
-				"""
-				locals()['runners%runnerId' % self.runnerId] = Label(self.runnersFrame, text = self.txt)
+		dataC = data()
+		self.canDb = dataC.openDataBase()
+		if(self.canDb=='True'):
+			self.conn = lite.connect('data/runner.db')
+			self.c = self.conn.cursor()
+			self.c.execute("select * from Identification")
+			self.data = self.c.fetchall()
+			for elem in self.data:
+				self.output = 'ID:'+ elem[0]+' FName: '+ elem[1]+' LName: '+elem[2]
+				self.runnerId = elem[0]
+				locals()['runners%runnerId' % self.runnerId] = Label(self.runnersFrame, text = self.output)
 				locals()['runners%runnerId' % self.runnerId].grid(row = self.rowNum)
-				"""
-				The debate is whether to create each runner graphic here, or not... it seenms logical todo so...
-				"""
-				
-				self.runnerId = self.runnerId + 1
 				self.rowNum = self.rowNum + 1
-			else:
-				pass
-		
+		if(self.canDb=='False'):
+			self.failureT = Label(self.runnersFrame, text='Failure -- Could not find db, or we has a importation error')
+			self.failureT.grid()
+		"""
+		Team stats
+		"""
+		if(self.canDb=='True'):
+			self.teamRaceAdv = dataC.fetchTeamAdv()
+			self.raceId = 1
+			self.rowNum = 0
+			for items in self.teamRaceAdv:
+				locals()['race%raceId' % self.raceId] = Label(self.teamStatsFrame, text = 'Race'+str(self.raceId)+': '+str(items))
+				locals()['race%raceId' % self.raceId].grid(row = self.rowNum)
+				self.raceId = self.raceId + 1
+				self.rowNum = self.rowNum + 1
+		if(self.canDb=='False'):
+			self.failureT2 = Label(self.runnersFrame, text='Failure -- Could not find db, or we has a importation error')
+			self.failureT2.grid()
 		print('['+'\033[31m'+'ProjectRunner'+'\033[0m'+']'+'Build successfull...')
 	def buildRunners(self):
 		print('['+'\033[31m'+'ProjectRunner'+'\033[0m'+']'+'start of runner GUI')
@@ -108,47 +120,63 @@ class data:
 		Here is where we should bring in our databases, and any other data bases.
 		"""
 
-
-	def openJson(self):
-		print('['+'\033[31m'+'ProjectRunner'+'\033[0m'+']'+'Attempting to open the Db...')
-		with open("data.json", "r") as self.dataFile:
-			self.dbData = json.load(self.dataFile)
-			
-	def addDataToJson(self):
-		print('['+'\033[31m'+'ProjectRunner'+'\033[0m'+']'+'Creating and populating a JSON file for use a temp data base...')
-		with open("data.json", "w") as self.dataFile:
-			json.dump({'teamId':'12', '1':'joe', '2':'rick', '3':'phfill'}, self.dataFile, indent = 4)
-	def getTeamId(self):
-		print('['+'\033[31m'+'ProjectRunner'+'\033[0m'+']'+'Fetching team id...')
+	def openDataBase(self):
+		print('['+'\033[31m'+'ProjectRunner'+'\033[0m'+']'+'Attempting to open up the data base...')
 		try:
-			self.teamId = self.dbData['teamId']
-			print('['+'\033[31m'+'ProjectRunner'+'\033[0m'+']'+'Successful fetching. Returning data now...')
-			return self.teamId
+
+			self.conn = lite.connect('data/runner.db')
+			self.c = self.conn.cursor()
+
+			print('['+'\033[31m'+'ProjectRunner'+'\033[0m'+']'+'Data Base successfully opened...')
+			self.c.close()
+			return 'True'
 		except:
-			print('['+'\033[31m'+'ProjectRunner'+'\033[0m'+']'+'Either data fetch failed, or json file is not populated... Going to attempt to open json...')
-			try:
-				self.openJson()
-				self.teamId = self.dbData['teamId']
-				return self.teamId
-				pass
-			except:
-				print('['+'\033[31m'+'ProjectRunner'+'\033[0m'+']'+'Attempting to populate the JSON file...')
-				self.addDataToJson()
-				self.teamId = self.dbData['teamId']
-				return self.teamId				
-	def getRunners(self):
-		self.runnerList = []
-
-		self.r1 = self.dbData['1']
-		self.r2 = self.dbData['2']
-		self.r3 = self.dbData['3']
-		self.runnerList.insert(0, self.r1)
-		self.runnerList.insert(1, self.r2)
-		self.runnerList.insert(2, self.r3)
-		self.runnerList.insert(3, 'null')
-		return self.runnerList
-
-
+			print('Could not connect to db file, make sure it exsists in the data folder, and runner.db is inside of that folder. Or just change the file pointer :shrug:')
+			return 'False'
+	def fetchTeamId(self):
+		self.canDb = self.openDataBase()
+		if(self.canDb=='True'):
+			self.conn = lite.connect('data/runner.db')
+			self.c = self.conn.cursor()
+			self.c.execute("select * from teamData")
+			self.data = self.c.fetchone()
+			for elem in self.data:
+				self.teamId = elem[0]
+				break
+			return self.teamId
+	def fetchTeamAdv(self):
+		dataList = []
+		self.advNum = 0
+		self.totalNum = 0
+		self.conn = lite.connect('data/runner.db')
+		self.c = self.conn.cursor()
+		self.c.execute("select * from Stats")
+		self.data = self.c.fetchall()
+		self.race = 0
+		for self.items in self.data:
+		
+			for self.time in self.data:
+				self.strTime = str(self.time[self.race])
+				m, s = self.strTime.split(':')
+				self.total = int(m)*60
+				self.total = self.total+int(s)
+				self.advNum = int(self.advNum) + int(self.total)
+				self.totalNum = int(self.totalNum) + 1
+			print(self.advNum)
+			print(self.totalNum)
+			self.raceAdv = int(self.advNum) / int(self.totalNum)
+			print(self.raceAdv)
+			self.mins = str(int(self.raceAdv) / 60)
+			self.mins, self.undeeded = self.mins.split('.')
+			self.mins = str(self.mins)
+			self.seconds = int(int(self.mins) * 60)
+			self.seconds = int(self.raceAdv - self.seconds)
+			self.seconds = str(self.seconds)
+			print('Race '+str(self.race+1)+': '+self.mins+':'+self.seconds)
+			self.time = str(self.mins)+':'+str(self.seconds)
+			dataList.insert(self.race, self.time)
+			self.race = self.race + 1
+		return dataList	
 
 guiC = gui()
 guiC.mainPage()
