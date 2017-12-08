ff55887e1afee9ee9289076bbc74cd1780c3444b
JC TECH <33255294+JCTLearning@users.noreply.github.com>
2017-12-08 18:15:07 -0500

    Deleted unneeded script

--- xml/dataToXml.py
+++
@@ -1,9 +0,0 @@
-from xml.etree import ElementTree as xml
-
-tree = xml.parse("file.xml", "w")
-
-xmlRoot = tree.getroot()
-child = xml.Element("file")
-xmlRoot.append(child)
-
-tree.write("file.xml", "w")
