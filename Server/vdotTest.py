fbd238560803923eede26106e3042d1ab2b20357
JC TECH <33255294+JCTLearning@users.noreply.github.com>
2018-01-12 08:07:57 -0500

    added vdot test

---
+++ Server/vdotTest.py
@@ -0,0 +1,84 @@
+## -- Fetch Vdot Nums -- #
+
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+class vdot:
+    def __init__(self):
+        print('[-- VDOT CALLED --]')
+    def vdotMiles(self, runnerTime):
+        scope = ['https://spreadsheets.google.com/feeds']
+        credentials = ServiceAccountCredentials.from_json_keyfile_name('authfile.json', scope)
+        gc = gspread.authorize(credentials)
+        sheet = '13UTcj1AKMIZ-cCYlKQVIxaYdr8TmOuX43HVw0l0KYmE'
+        wks = gc.open_by_key(sheet)
+        worksheet = wks.worksheet("VDOT")
+        db = []
+        vdotNum = 85
+        loopNum = 0
+        for items in worksheet.col_values(4): #4 is miles
+            if(items):
+                if(items=='Mile'): # gets rid of row one -- mile
+                    pass
+                else:
+                    #print(items)
+                    listV = []
+                    listV.insert(0, items)
+                    listV.insert(1, vdotNum)
+                    db.insert(loopNum, listV)
+                    vdotNum = vdotNum - 1
+                    loopNum = loopNum + 1
+            else:
+                break
+        Vmiles = runnerTime # User mile time
+        output = []
+        x = 0
+        for miles in db:
+            #print(miles)
+            #print('x'+miles[0])
+            x, y = miles[0].split(':')
+            mileTime = int(x)*60
+            mileTime = mileTime + int(y)
+            print('VDOTNUMBER: '+str(miles[1])+' ITS SECONDS: '+str(mileTime))
+            list = []
+            """
+            x is the math
+            """
+            x = int(Vmiles) - int(mileTime)
+            x = str(x)
+            x = x.replace('-', '')
+            print('USERTIME: '+str(Vmiles)+' - VDOT SECONDS: '+ str(mileTime)+' THE DISTANCE BETWEEN THE TWO: '+str(x))
+
+            x = int(x)
+            list.insert(0, x) #subtracted time
+            list.insert(1, miles[1]) #vdot
+            output.insert(x, list)
+            x = x + 1
+        """
+        Sorting --  what need to happen here is that each file is deleted upon the script running, but we store the testDb.db in the user fiie. Lets also name it mile or km depending on value.
+        """
+        conn = lite.connect('testDb.db')
+        c = conn.cursor()
+        c.execute("create table data(vdot int, time int)")
+        for items in output:
+            #each item is now a list that contains time and vdot soooo
+            c.execute("insert into data(vdot, time) values (?, ?)', (items[1], items[0]) )")
+        conn.commit()
+        c.close()
+        #grab Data
+        conn = lite.connect('testDb.db')
+        c = conn.cursor()
+        c.execute('select vdot, time from data sort by time asc')
+        returnSql = c.fetchall()
+
+
+        vdot = returnSql[0] #selects the first value
+        return vdot #this is the vdot for that number
+runnerTime = input('Mile Time: ')
+x, y = runnerTime.split(':')
+runnerTime = int(x) * 60
+runnerTime = runnerTime + int(y)
+vdotC = vdot()
+selfs = None
+vdotNum = vdot.vdotMiles(selfs, runnerTime)
+print(vdotNum)
+print('The VDOT for time: '+str(runnerTime)+' is: '+str(vdotNum[0]))
