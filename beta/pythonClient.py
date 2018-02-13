1f0601781e038a7c8fceeeab04ba21cf50c14e07
JC TECH <33255294+JCTLearning@users.noreply.github.com>
2018-02-13 13:45:21 +0000

    Add files via upload

--- beta/pythonClient.py
+++ beta/pythonClient.py
@@ -1,1 +1,17 @@
 import sqlite3 as lite
+
+
+class vdot:
+    def __init__(self):
+        pass
+    def fetchVdotNum(self, type, time):
+        conn = lite.connect('vdotDb.db')
+        c = conn.cursor()
+        c.execute("select * from vdot")
+        data = c.fetchall()
+        for items in data:
+            print("num: "+str(items[type]))
+            print("VDOT: "+str(items[0]))
+
+vdotC = vdot()
+vdot.fetchVdotNum(self, '1', '10:00')
