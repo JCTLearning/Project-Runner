e0d32dcb6955f8df1aa8406c7c925c88dcab6cee
JC TECH <33255294+JCTLearning@users.noreply.github.com>
2018-02-13 17:15:26 +0000

    built the entire back end, input needs edits

--- beta/pythonClient.py
+++ beta/pythonClient.py
@@ -1,17 +1,49 @@
 import sqlite3 as lite
-
+import os
 
 class vdot:
     def __init__(self):
         pass
-    def fetchVdotNum(self, type, time):
+    def fetchVdotNum(types, time):
         conn = lite.connect('vdotDb.db')
         c = conn.cursor()
         c.execute("select * from vdot")
+        mins, secs = time.split(':')
+        time = int(secs) + int(mins) * 60
         data = c.fetchall()
+        bigL = []
+        x = 0
         for items in data:
-            print("num: "+str(items[type]))
+            types1 = items[int(types)]
+            mins, secs = types1.split(':')
+            vdotTime = int(int(mins) * 60) + int(secs)
+            vdotNum = str(items[0])
+            difference = str(int(vdotTime) - int(time))
+            difference = difference.replace('-','')
+            listS = [difference, vdotNum]
+            bigL.insert(x, listS)
             print("VDOT: "+str(items[0]))
-
+            x = x + 1
+        print(bigL)
+        try:
+            os.remove('vdot.db')
+        except:
+            pass
+        conn = lite.connect('vdot.db')
+        c = conn.cursor()
+        c.execute("create table data(vdot int, time int)")
+        for items in bigL:
+            print(items)
+            #each item is now a list that contains time and vdot soooo
+            c.execute("insert into data(vdot, time) values (?, ?)", (items[1], items[0]) )
+        conn.commit()
+        c.close()
+        #grab Data
+        conn = lite.connect('vdot.db')
+        c = conn.cursor()
+        c.execute('select vdot, time from data order by time asc')
+        returnV = c.fetchall()
+        return returnV
 vdotC = vdot()
-vdot.fetchVdotNum(self, '1', '10:00')
+x = vdot.fetchVdotNum('1', '5:18')
+print(x[0])
